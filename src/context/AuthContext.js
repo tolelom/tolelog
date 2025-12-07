8c54bdf24abb59be2ed1c31c5c688a7e817f0749
김성민 <98kimsungmin@naver.com>
2025-12-07 19:13:11 +0900

    fix: AuthContext - userId 축소 추가

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,3 +1,9 @@
-import {createContext} from "react";
+import { createContext } from 'react';
 
-export const AuthContext = createContext();
+export const AuthContext = createContext({
+    token: null,
+    username: null,
+    userId: null,
+    login: () => {},
+    logout: () => {},
+});
