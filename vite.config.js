278a8bd2f8312cc86a19caafe30968efdd87958d
김성민 <98kimsungmin@naver.com>
2025-12-07 17:28:13 +0900

    config: Vite API 프롱싱 설정 모렇

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,18 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 80,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true,
-                secure: false,
-            }
-        }
+  plugins: [react()],
+  server: {
+    port: 5173,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
     }
+  }
 })
